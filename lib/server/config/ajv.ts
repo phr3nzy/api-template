894a66e0696bc1cc08a064daf9378fcf9b8d45b1
phr3nzy <adilosama47@gmail.com>
2022-06-11 16:25:51 +0200

    add logging to ajv instance

--- lib/server/config/ajv.ts
+++ lib/server/config/ajv.ts
@@ -1,12 +1,24 @@
 import Ajv from 'ajv';
 import applyErrors from 'ajv-errors';
 import applyFormats from 'ajv-formats';
+import logger from './logger';
 
 const ajv = new Ajv({
 	allErrors: true,
 	removeAdditional: true,
 	useDefaults: true,
 	coerceTypes: true,
+	logger: {
+		log(...args) {
+			logger.info(args);
+		},
+		error(...args) {
+			logger.error(args);
+		},
+		warn(...args) {
+			logger.warn(args);
+		},
+	},
 });
 
 applyFormats(ajv);
