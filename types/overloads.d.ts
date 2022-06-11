a7b3af0659faaa15f0b5b7f4ea0e48c08c6358dd
phr3nzy <adilosama47@gmail.com>
2022-06-11 16:26:33 +0200

    fix extending fastify instance type

--- types/overloads.d.ts
+++ types/overloads.d.ts
@@ -1,31 +1,26 @@
-import { FastifyInstance as DefaultFastifyInstance } from 'fastify';
 import { Redis } from 'ioredis';
 
 type ENV = 'development' | 'staging' | 'testing' | 'production';
 
+type EnvSchema = {
+	SERVICE_NAME: string;
+	NODE_ENV: ENV;
+	APP_ENV: ENV;
+	REDIS_URL: string;
+	LOG_LEVEL: 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
+	DISABLE_LOGGING: boolean;
+	HOST: string;
+	PORT: number;
+};
+
 declare module 'fastify' {
-	interface FastifyInstance extends DefaultFastifyInstance {
-		env: {
-			SERVICE_NAME: string;
-			NODE_ENV: ENV;
-			APP_ENV: ENV;
-			REDIS_URL: string;
-			LOG_LEVEL: 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
-			DISABLE_LOGGING: boolean;
-			HOST: string;
-			PORT: number;
-		};
-		config: {
-			SERVICE_NAME: string;
-			NODE_ENV: ENV;
-			APP_ENV: ENV;
-			REDIS_URL: string;
-			LOG_LEVEL: 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
-			DISABLE_LOGGING: boolean;
-			HOST: string;
-			PORT: number;
-		};
+	interface FastifyInstance {
+		env: EnvSchema;
+		config: EnvSchema;
 		cache: Redis;
-		swaggerCSP: {};
+		swaggerCSP: {
+			script: string[];
+			style: string[];
+		};
 	}
 }
