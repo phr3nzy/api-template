441ab6674fc680af2d335e25b38a85935bbf795a
phr3nzy <adilosama47@gmail.com>
2022-06-15 16:39:04 +0200

    add server start file

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,24 @@
-console.log('Just getting started...');
+import 'make-promises-safe';
+import { bootstrap } from './server';
+
+/**
+ * Starts the API Service
+ */
+async function start() {
+	try {
+		// Bootstrap the server
+		const server = await bootstrap();
+
+		// Catch and log any errors that occur during plugin registration
+		await server.ready();
+
+		// Start listening for requests
+		await server.listen({ host: server.config.HOST, port: server.config.PORT });
+		server.log.debug(server.printRoutes());
+	} catch (error) {
+		// Log uncaught startup errors
+		console.error(error);
+	}
+}
+
+start();
